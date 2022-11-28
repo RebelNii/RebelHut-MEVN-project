a9ccee63bfb54ccfb1cedd209e187cd91f0e1157
Kwade Jeffrey <kwadejeffrey@gmail.com>
2022-11-28 15:59:17 +0000

    upd

--- src/components/notesData.vue
+++ src/components/notesData.vue
@@ -3,7 +3,7 @@
         <div class="viewNotes">
             <note class="grid md:grid-cols-2 gap-10">
                 <div v-for="(notes , index) in allNotes" :key="index" class="card  h-60 bg-slate-500 rounded">
-                    <div class="card-1 bg-teal-300 text-stone-900 py-3 px-2 ">
+                    <div class="card-1 bg-teal-300 text-stone-900 py-3 px-2 h-full">
                         <div class="my-4 text-center">
                             <span class="text-base font-medium">Assigned to</span> <br>
                             <span class="font-medium text-sm">{{notes.username}}</span>
@@ -18,14 +18,14 @@
                         </div>
                     </div>
                     <div class="card-2 bg-purple-300 text-stone-800 text-center break-words py-3 px-4">
-                        <div class="title my-3">
+                        <div class="title mb-1">
                             <h4 class="text-lg font-medium">{{notes.title}}</h4>
                         </div>
-                        <div class="my-3">
+                        <div class="m-1">
                             <p class="text-sm">{{notes.text}}</p>
                         </div>
-                        <div class="mt-3 cursor-pointer w-full">
-                            <router-link class="bg-green-500 transition-all hover:bg-green-300  p-1 w-fit rounded text-sm uppercase" :to="'/update-note/'+notes._id">Update</router-link>
+                        <div class="mt-2 cursor-pointer w-full">
+                            <router-link class="hover:bg-green-500 transition-all bg-green-400  p-1 w-fit rounded text-sm uppercase" :to="'/update-note/'+notes._id">Update</router-link>
                         </div>
                     </div>
                 </div>
--- src/store/index.js
+++ src/store/index.js
@@ -1,233 +1,281 @@
-import { createStore } from 'vuex'
-import axios from 'axios'
-import createPersistedState from 'vuex-persistedstate';
+import { createStore } from "vuex";
+import axios from "axios";
+import createPersistedState from "vuex-persistedstate";
 import jwt_decode from "jwt-decode";
-import router from '../router/index'
+import router from "../router/index";
+import SecureLS from "secure-ls";
+
+let ls = new SecureLS({
+  encodingType: "aes",
+  isCompression: false,
+  encryptionSecret: "hello",
+});
 
 export default createStore({
   state: {
     notes: [],
     users: [],
     token: "",
-    person: '',
+    person: "",
     active: null,
-    decoded: '',
-    roles: '',
+    decoded: "",
+    roles: "",
     Admin: null,
     Manager: null,
     Employee: null,
-    auth: null
+    auth: null,
   },
   getters: {
-    allNotes(state){
-      return state.notes
+    allNotes(state) {
+      return state.notes;
     },
 
-    getAllUsers(state){
-      return state.users
+    getAllUsers(state) {
+      return state.users;
     },
 
-    getAdmin(state){
-      state.Admin
+    getAdmin(state) {
+      state.Admin;
     },
 
-    getManager(state){
-      state.Manager
-    }
+    getManager(state) {
+      state.Manager;
+    },
   },
   mutations: {
-    setNotes(state,payload){
-      state.notes = payload
+    setNotes(state, payload) {
+      state.notes = payload;
     },
 
-    setUser(state,payload){
-      state.users = payload
+    setUser(state, payload) {
+      state.users = payload;
     },
 
-    removeUser(state,id){
-      state.users = state.users.filter((user) => user._id !== id)
+    removeUser(state, id) {
+      state.users = state.users.filter((user) => user._id !== id);
     },
 
-    removeNote(state, id){
-      state.notes = state.users.filter((note) => note._id !== id)
+    removeNote(state, id) {
+      state.notes = state.users.filter((note) => note._id !== id);
     },
 
-    setToken(state,payload){
-      state.token = payload
+    setToken(state, payload) {
+      state.token = payload;
     },
 
-    setPerson(state,payload){
-      state.person = payload
+    setPerson(state, payload) {
+      state.person = payload;
       state.active = true;
       state.roles = payload.roles;
-      state.auth = true
-      if(payload.roles.includes('Employee')){
-        state.Employee = true
+      state.auth = true;
+      if (payload.roles.includes("Employee")) {
+        state.Employee = true;
         // console.log({'E':state.Employee})
       }
 
-      if(payload.roles.includes('Manager')){
-        state.Manager = true
-        console.log({'M':state.Manager})
+      if (payload.roles.includes("Manager")) {
+        state.Manager = true;
+        console.log({ M: state.Manager });
       }
 
-      if(payload.roles.includes('Admin')){
-        state.Admin = true
-        console.log({'A':state.Admin})
+      if (payload.roles.includes("Admin")) {
+        state.Admin = true;
+        console.log({ A: state.Admin });
       }
     },
 
-    setDecoded(state, payload){
-      state.decoded = payload
+    setDecoded(state, payload) {
+      state.decoded = payload;
     },
 
-    removePerson(state){
-      state.person = '';
+    removePerson(state) {
+      state.person = "";
       state.active = null;
       state.decoded = null;
-      state.roles = '',
-      state.Admin = null
-      state.Employee = null
-      state.Manager = null
-      state.auth = null
+      (state.roles = ""), (state.Admin = null);
+      state.Employee = null;
+      state.Manager = null;
+      state.auth = null;
     },
-
   },
   actions: {
-    async fetchUsers({commit}){
-      const res = await axios.get('https://rebelhut-api.onrender.com/users',{
-        headers:{
-          Authorization: `Bearer ${localStorage.getItem('token')}`
+    async fetchUsers({ commit }) {
+      const res = await axios.get("https://rebelhut-api.onrender.com/users", {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
         },
         withCredentials: true,
-        credentials: 'include'
-      })
+        credentials: "include",
+      });
       // console.log(res.data)
-      commit('setUser',res.data)
+      commit("setUser", res.data);
     },
 
-    async regUser({commit},{username,password,roles}){
-      const res = await axios.post('https://rebelhut-api.onrender.com/users',{username,password,roles},{
-        headers:{
-          Authorization: `Bearer ${localStorage.getItem('token')}`
-        },
-        withCredentials: true,
-        credentials: 'include'
-      })
+    async regUser({ commit }, { username, password, roles }) {
+      const res = await axios.post(
+        "https://rebelhut-api.onrender.com/users",
+        { username, password, roles },
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+          withCredentials: true,
+          credentials: "include",
+        }
+      );
       // console.log(res.data)
     },
 
-    async updateUser({commit},{id,username,password,roles,active}){
-      const res = await axios.patch('https://rebelhut-api.onrender.com/users',{id,username,password,roles,active},{
-        headers:{
-          Authorization: `Bearer ${localStorage.getItem('token')}`
-        },
-        withCredentials: true,
-        credentials: 'include'
-      })
+    async updateUser({ commit }, { id, username, password, roles, active }) {
+      const res = await axios.patch(
+        "https://rebelhut-api.onrender.com/users",
+        { id, username, password, roles, active },
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+          withCredentials: true,
+          credentials: "include",
+        }
+      );
       // console.log(res.data)
-      commit('setUser',res.data)
+      commit("setUser", res.data);
     },
 
-    async deleteUser({commit},id){
-      const res = await axios.delete(`https://rebelhut-api.onrender.com/users/${id}`,{
-        headers:{
-          Authorization: `Bearer ${localStorage.getItem('token')}`
-        },
-        withCredentials: true,
-        credentials: 'include'
-      })
+    async deleteUser({ commit }, id) {
+      const res = await axios.delete(
+        `https://rebelhut-api.onrender.com/users/${id}`,
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+          withCredentials: true,
+          credentials: "include",
+        }
+      );
       // console.log(res.data)
       // console.log(id)
-      commit('removeUser', id)
+      commit("removeUser", id);
     },
 
-    async getSingleUser({commit},id){
-      const res = await axios.get(`https://rebelhut-api.onrender.com/users/${id}`)
+    async getSingleUser({ commit }, id) {
+      const res = await axios.get(
+        `https://rebelhut-api.onrender.com/users/${id}`
+      );
       // console.log(res.data)
-      return res.data
+      return res.data;
     },
 
-    async loginUser({commit},{username,password}){
-      const {data} = await axios.post('https://rebelhut-api.onrender.com/auth',{username,password},{
-        withCredentials: true,
-        credentials: 'include'
-      })
+    async loginUser({ commit }, { username, password }) {
+      const { data } = await axios.post(
+        "https://rebelhut-api.onrender.com/auth",
+        { username, password },
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+          withCredentials: true,
+          credentials: "include",
+        }
+      );
       // console.log(data)
       // console.log(data.accessToken)
       // console.log(jwt_decode(data.accessToken))
-      let token = jwt_decode(data.accessToken)
+      let token = jwt_decode(data.accessToken);
       // console.log(token.UserInfo)
       // console.log(data.person.id)
-      commit('setToken',data.accessToken)
+      commit("setToken", data.accessToken);
       // commit('setPerson',data.person)
-      commit('setPerson',token.UserInfo)
-      commit('setDecoded',jwt_decode(data.accessToken))
+      commit("setPerson", token.UserInfo);
+      commit("setDecoded", jwt_decode(data.accessToken));
 
       // axios.defaults.headers.common['Authorization'] = `Bearer ${data.accessToken}`
-      localStorage.setItem('token',data.accessToken)
+      localStorage.setItem("token", data.accessToken);
 
-      return data
+      return data;
     },
 
-    async logout({commit}){
-      const {data} = await axios.post('https://rebelhut-api.onrender.com/auth/logout')
-      localStorage.removeItem('token')
-      commit('removePerson')
-      router.push('/')
+    async logout({ commit }) {
+      const { data } = await axios.post(
+        "https://rebelhut-api.onrender.com/auth/logout"
+      );
+      localStorage.removeItem("token");
+      commit("removePerson");
+      router.push("/");
     },
 
-    async addNote({commit},{user, title, text}){
-      const res = await axios.post('https://rebelhut-api.onrender.com/notes',{user,title,text},{
-        headers:{
-          Authorization: `Bearer ${localStorage.getItem('token')}`
-        },
-        withCredentials: true,
-        credentials: 'include'
-      })
-      console.log(res)
-
+    async addNote({ commit }, { user, title, text }) {
+      const res = await axios.post(
+        "https://rebelhut-api.onrender.com/notes",
+        { user, title, text },
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+          withCredentials: true,
+          credentials: "include",
+        }
+      );
+      console.log(res);
     },
 
-    async fetchNotes({commit}){
-      const {data} = await axios.get('https://rebelhut-api.onrender.com/notes',{
-        headers:{
-          Authorization: `Bearer ${localStorage.getItem('token')}`
-        },
-        withCredentials: true,
-        credentials: 'include'
-      });
+    async fetchNotes({ commit }) {
+      const { data } = await axios.get(
+        "https://rebelhut-api.onrender.com/notes",
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+          withCredentials: true,
+          credentials: "include",
+        }
+      );
 
-      console.log(data)
-      commit('setNotes',data)
+      console.log(data);
+      commit("setNotes", data);
     },
 
-    async updateNote({commit},{id,user,title, text, completed}){
-      const {data} = await axios.patch('https://rebelhut-api.onrender.com/notes',{id,user,title,text,completed},{
-        headers:{
-          Authorization: `Bearer ${localStorage.getItem('token')}`
-        },
-        withCredentials: true,
-        credentials: 'include'
-      })
-      console.log(data)
-      router.push('/dash')
+    async updateNote({ commit }, { id, user, title, text, completed }) {
+      const { data } = await axios.patch(
+        "https://rebelhut-api.onrender.com/notes",
+        { id, user, title, text, completed },
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+          withCredentials: true,
+          credentials: "include",
+        }
+      );
+      console.log(data);
+      router.push("/dash");
     },
 
-    async deleteNote({commit},id){
-      const {data} = await axios.delete(`https://rebelhut-api.onrender.com/notes/${id}`,{
-        headers:{
-          Authorization: `Bearer ${localStorage.getItem('token')}`
-        },
-        withCredentials: true,
-        credentials: 'include'
-      });
+    async deleteNote({ commit }, id) {
+      const { data } = await axios.delete(
+        `https://rebelhut-api.onrender.com/notes/${id}`,
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+          withCredentials: true,
+          credentials: "include",
+        }
+      );
 
-      commit('removeNote', id)
-      router.push('/dash')
-    }
+      commit("removeNote", id);
+      router.push("/dash");
+    },
   },
-  modules: {
-  },
-  plugins: [createPersistedState()],
-})
+  modules: {},
+  plugins: [
+    createPersistedState({
+      key: "EG",
+      storage: {
+        getItem: (key) => ls.get(key),
+        setItem: (key, value) => ls.set(key, value),
+        removeItem: (key) => ls.remove(key),
+      },
+    }),
+  ],
+});
